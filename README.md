7aaf2c3398a511cc63f1865b02b6976cb3df19b7
s-continuum, IT265 <noreply-jenkins@post.ch>
2022-06-30 09:19:44 +0200

    v7.0.1

--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 ## Releases
 
 - [v7.0.0](https://TechSysApi.github.io/vsi-process-control-stream-api-v2-dist/v7.0.0/ui/?url=../complete-api.yaml)
+- [v7.0.1](https://TechSysApi.github.io/vsi-process-control-stream-api-v2-dist/v7.0.1/ui/?url=../complete-api.yaml)
---
+++ v7.0.1/ui/patch/variables.js
@@ -0,0 +1,52 @@
+var variables={
+  "name": "@ch-post/vsi-process-control-stream-api-v2",
+  "version": "7.0.1",
+  "description": "API definition - Interaction between Machine Control and Reading Coding System - CEN/TS 16238 v2",
+  "scripts": {
+    "start": "apikana start src dist",
+    "stop": "apikana stop",
+    "create-sample": "apikana create-sample",
+    "validate-samples": "apikana validate-samples",
+    "test": "eslint src --ext .ts && apikana validate-samples",
+    "lint": "eslint src --ext .ts",
+    "lint-fix": "eslint src --ext .ts --fix"
+  },
+  "author": "herzamk",
+  "license": "Apache-2.0",
+  "dependencies": {
+    "@ch-post/cen-mc-rc-stream-api-v2": "^5.0.1"
+  },
+  "devDependencies": {
+    "apikana": "^0.9.25",
+    "apikana-defaults": "0.0.77",
+    "@typescript-eslint/eslint-plugin": "^4.33.0",
+    "@typescript-eslint/parser": "^4.33.0",
+    "eslint": "^7.17.0",
+    "eslint-config-prettier": "^7.1.0",
+    "eslint-plugin-prettier": "^3.3.1",
+    "prettier": "^2.2.1",
+    "typescript": "^4.1.3"
+  },
+  "customConfig": {
+    "type": "stream-api",
+    "domain": "post.ch",
+    "author": "herzamk",
+    "namespace": "app.techsys.vsi.process.control.v2",
+    "shortName": "vsi-process-control-v2",
+    "projectName": "vsi-process-control-stream-api-v2",
+    "npmPackage": "@ch-post/vsi-process-control-stream-api-v2",
+    "title": "VSI Process Control Stream API v2",
+    "plugins": [
+      "dotnet",
+      "readme"
+    ],
+    "dotnetNamespace": "Ch.Post.PL.Api.VsiProcessControl.V2",
+    "dotnetPackageId": "Ch.Post.PL.Api.VsiProcessControl.V2",
+    "mqs": "MQTT"
+  },
+  "_": [
+    "start",
+    "src",
+    "dist"
+  ]
+}
